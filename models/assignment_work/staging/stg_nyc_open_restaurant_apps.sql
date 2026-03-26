2ed2efa86e042ac95432beee3335343c52833e53
Nousin Rahman <nousin.rahman90@gmail.com>
2026-03-26 23:22:31 +0000

    update sql

--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -7,7 +7,6 @@ WITH source AS (
 
 cleaned AS (
    SELECT
-       -- Keep other columns except ones we clean
        * EXCEPT (
            objectid,
            restaurant_name,
@@ -23,7 +22,7 @@ cleaned AS (
            sla_license_type,
            application_status,
            application_type,
-           submission_timestamp
+           time_of_submission 
        ),
 
        -- Identifier
@@ -33,7 +32,7 @@ cleaned AS (
        TRIM(CAST(restaurant_name AS STRING)) AS restaurant_name,
        TRIM(CAST(legal_business_name AS STRING)) AS legal_business_name,
 
-
+       -- Borough
        CASE
            WHEN UPPER(TRIM(borough)) IN ('MANHATTAN', 'NEW YORK COUNTY') THEN 'Manhattan'
            WHEN UPPER(TRIM(borough)) IN ('BRONX', 'THE BRONX') THEN 'Bronx'
@@ -43,7 +42,7 @@ cleaned AS (
            ELSE 'UNKNOWN'
        END AS borough,
 
-       -- Zip Code Cleaning
+       -- Zip Code
        CASE
            WHEN LENGTH(CAST(zip_code AS STRING)) = 5 THEN CAST(zip_code AS STRING)
            ELSE NULL
@@ -53,35 +52,31 @@ cleaned AS (
        UPPER(TRIM(CAST(application_status AS STRING))) AS application_status,
        UPPER(TRIM(CAST(application_type AS STRING))) AS application_type,
 
-       -- Seating Info
+       -- Seating
        UPPER(TRIM(CAST(seating_interest AS STRING))) AS seating_interest,
        CAST(approved_for_sidewalk_seating AS STRING) AS sidewalk_seating_flag,
        CAST(approved_for_roadway_seating AS STRING) AS roadway_seating_flag,
 
-       -- SLA Info
+       -- SLA
        CAST(sla_serial_number AS STRING) AS sla_serial_number,
        CAST(sla_license_type AS STRING) AS sla_license_type,
 
-       -- Dates
-       CAST(submission_timestamp AS TIMESTAMP) AS submission_timestamp,
+ 
+       CAST(time_of_submission AS TIMESTAMP) AS submission_timestamp,
 
-       -- Metadata
        CURRENT_TIMESTAMP() AS _stg_loaded_at
 
    FROM source
 
-   -- Filters
    WHERE objectid IS NOT NULL
-     AND submission_timestamp IS NOT NULL
+     AND time_of_submission IS NOT NULL 
      AND borough IS NOT NULL
 
-   -- Deduplicate (latest application per ID)
    QUALIFY ROW_NUMBER() OVER (
        PARTITION BY objectid
-       ORDER BY submission_timestamp DESC
+       ORDER BY time_of_submission DESC   
 ) = 1
 )
-
 
 SELECT * FROM cleaned
 
